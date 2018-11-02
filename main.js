d0bc95a39e15fabfe33ae9c639de106faf4a5fa9
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-11-02 13:19:43 -0400

    really added crypto.js files

--- main.js
+++ main.js
@@ -4,19 +4,65 @@
 
 //shft alt f to tidy code.
 
-const SHA256 = require('crypto.js/sha256');
+const SHA256 = require('crypto.js/sha256.js');
+//gab! sha256 or sha256.js?
 
+//the basic block:
 class Block {
+    //what's in it:
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.has = '';
+        this.hash = this.calculateHash();
     }
 
+    //how it gets its own hash:
     calculateHash() {
         return SHA256(this.index + this.previousHash + this.timestamp
-            + JSON.stringify(this.data));
+            + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock];
+        //if you didn't create the first block manually
+        //this could be empty
+        //instead, you initialize this array with the first block in it.
+    }
+
+    //first block is called Genesis Block
+    //it must be created manually
+    //here's a method to do that:
+    createGenesisBlock(){
+        return new Block(0, "01/01/2018", "Genesis Block data here.", "0");
+        //see how this is creating one instance of your class Block?
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain - 1];
+        //ie, array index of the last block
+    }
+
+    //this method instructs the program what to do to add a new block
+    //IRL it would be more complicated, with checks, etc
+    //but this is fine for your first one.
+    addBlock(){
+        //set the previosHash, which is the current hash of the last block"
+        newBlock.previousHash = this.getLatestBlock().hash;
+        //calc the hash for the new block:
+        newBlock.hash = newBlock.calculateHash();
+        //add the new block to the chain:
+        this.chain.push(newBlock);
+    }
+}
+
+//let's test by creating an instance:
+
+let gabGeeCoin = new Blockchain();
+gabGeeCoin.addBlock(new Block(1, "10/25/2018", {amount: 5}));
+gabGeeCoin.addBlock(new Block(2, "10/28/2018", {amount: 8}));
+
+console.log(JSON.stringify(gabGeeCoin, null, 4));
